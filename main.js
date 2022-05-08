4d7ec6ebea10b1e93a10173630070cdb22873a4c
Clerigou <clerissonlins797@gmail.com>
2022-05-08 20:57:50 -0300

    Fourth day

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
-    if(scrollY > 0) {
-  navigation.classList.add('scroll')
-    }
-   else{
-       navigation.classList.remove('scroll')
-   }
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+function showNavOnScroll(){
+  if(scrollY > 0) {
+    navigation.classList.add('scroll')
+      }
+     else{
+         navigation.classList.remove('scroll')
+     }
+}
+function showBackToTopButtonOnScroll() {
+  if(scrollY > 450) {
+    backToTopButton.classList.add('show')
+      }
+     else{
+         backToTopButton.classList.remove('show')
+     }
 }
 
 function openMenu() {
